9ec2cbad8963bc2ae27a94f71dfafd645e88993e
reza <102231043+rezaluthfi@users.noreply.github.com>
2023-12-28 00:26:42 +0700

    -initial commit

---
+++ main.js
@@ -0,0 +1,97 @@
+//change navbar styles on scroll
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scroll", window.scrollY > 0);
+});
+
+//show or hide faq answer
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
+
+    //change icon when it open
+    const icon = faq.querySelector(".faq_icon i");
+    if (icon.className === "uil uil-plus") {
+      icon.className = "uil uil-minus";
+    } else {
+      icon.className = "uil uil-plus";
+    }
+  });
+});
+
+//show/hide nav menu
+const menu = document.querySelector(".nav_menu");
+const menuBtn = document.querySelector("#open-menu-button");
+const closeBtn = document.querySelector("#close-menu-button");
+
+menuBtn.addEventListener("click", () => {
+  menu.style.display = "flex";
+  closeBtn.style.display = "inline-block";
+  menuBtn.style.display = "none";
+});
+
+//close nav menu
+const closeNav = () => {
+  menu.style.display = "none";
+  closeBtn.style.display = "none";
+  menuBtn.style.display = "inline-block";
+};
+
+closeBtn.addEventListener("click", closeNav);
+
+//gallery change nav active
+let list = document.querySelectorAll(".list");
+let itemBox = document.querySelectorAll(".itemBox");
+
+for (let i = 0; i < list.length; i++) {
+  list[i].addEventListener("click", function () {
+    for (let j = 0; j < list.length; j++) {
+      list[j].classList.remove("active");
+    }
+    this.classList.add("active");
+
+    let dataFilter = this.getAttribute("data-filter");
+
+    for (let k = 0; k < itemBox.length; k++) {
+      itemBox[k].classList.remove("active");
+      itemBox[k].classList.add("hide");
+
+      if (
+        itemBox[k].getAttribute("data-item") == dataFilter ||
+        dataFilter == "all"
+      ) {
+        itemBox[k].classList.remove("hide");
+        itemBox[k].classList.add("active");
+      }
+    }
+  });
+}
+
+//Scroll Top
+let scrollTop = document.querySelector(".scroll-top");
+
+window.addEventListener("scroll", () => {
+  scrollTop.classList.toggle("scroll-active", window.scrollY >= 350);
+});
+
+//Scroll Reveal Animation
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  //reset: true
+});
+
+sr.reveal(".header_left", { delay: 600, origin: "left" });
+sr.reveal(".header_right", { delay: 600, origin: "right" });
+sr.reveal(".about_achievements-left", { delay: 600, origin: "top" });
+sr.reveal(".about_achievements-right", { delay: 600, origin: "bottom" });
+sr.reveal(".about_coorperation-left", { delay: 600, origin: "bottom" });
+sr.reveal(".about_coorperation-right", { delay: 600, origin: "top" });
+sr.reveal(".course_left", { delay: 600, origin: "left" });
+sr.reveal(".course_right", { delay: 600, origin: "right" });
+sr.reveal(".faq", { delay: 600, origin: "top" });
